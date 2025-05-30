a74e5a1a04fc509ababf3f08db68a6b5c4b31274
JuliaZzy <867306335@qq.com>
2025-05-30 11:26:16 +0800

    Update AdminPage.vue

--- src/views/AdminPage.vue
+++ src/views/AdminPage.vue
@@ -72,11 +72,11 @@
                         />
                         <div class="filter-options">
                           <label
-                            v-for="val in getFilteredOptions(col)"
+                            v-for="val in getFilteredOptions(colKey)"
                             :key="val"
                           >
-                            <input type="checkbox" :value="val" v-model="pendingFilters[col]" />
-                            {{ col === 'status' ? (statusDisplayMap[val] || val) : val }}
+                            <input type="checkbox" :value="val" v-model="pendingFilters[colKey]" />
+                            {{ colKey === 'status' ? (statusDisplayMap[val] || val) : val }}
                           </label>
                         </div>
                         <div class="filter-footer">
