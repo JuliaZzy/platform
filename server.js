b6a2be9321ca5920b79817f2ebb6db1876881444
JuliaZzy <867306335@qq.com>
2025-06-19 14:52:44 +0800

    correct the server.js

--- server.js
+++ server.js
@@ -1,7 +1,5 @@
 // 1. 环境变量 + 初始化
-if (process.env.NODE_ENV !== 'production') {
-  require('dotenv').config();
-}
+require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const path = require('path');
